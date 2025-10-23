9ef46be19e7fe6ecb78e3687289a5155fb796fb7
graceworl56 <anaghamullassery2001@gmail.com>
2025-10-23 10:17:05 +0530

    react1

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// import Button from '@mui/material/Button';
+// import { FaHome } from "react-icons/fa";
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+    {/* <h1>RESUME  BUILDER  <FaHome /> </h1>
+     <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button> */}
+
+    </>
+  )
+}
+
+export default App
